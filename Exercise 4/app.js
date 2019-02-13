1135e384984b541fe6bb58a976308e3f3cd2a6a6
Justin Bettencourt <justinvbettencourt@gmail.com>
2019-02-13 15:41:03 -0500

    Exercise 4 started.

---
+++ Exercise 4/app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#exercise',
+  data: {
+	effectClasses: {
+		highlight: false,
+		shrink: true
+	}
+  },
+  methods: {
+    startEffect: function() {
+		var vm = this;
+		setInterval(function() {
+			vm.effectClasses.highlight = !vm.effectClasses.highlight,
+			vm.effectClasses.shrink = !vm.effectClasses.shrink
+		}, 1000);
+    }
+  }
+});
